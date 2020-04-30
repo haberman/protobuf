9abec8fd8e001403df7f99150f89886b5d7bd0ab
Joshua Haberman <jhaberman@gmail.com>
2020-04-30 14:18:09 -0700

    Simplified PHP testing scripts. Deleted unused tests.

--- php/tests/test.sh
+++ php/tests/test.sh
@@ -1,41 +1,50 @@
 #!/bin/bash
 
-VERSION=$1
+set -ex
 
-export PATH=/usr/local/php-$VERSION/bin:$PATH
-export C_INCLUDE_PATH=/usr/local/php-$VERSION/include/php/main:/usr/local/php-$VERSION/include/php:$C_INCLUDE_PATH
-export CPLUS_INCLUDE_PATH=/usr/local/php-$VERSION/include/php/main:/usr/local/php-$VERSION/include/php:$CPLUS_INCLUDE_PATH
+PHP_BASE=$1
+PHP=$PHP_BASE/bin/php
+
+# Newest version that still supports PHP 7.2
+# But throws lots of deprecation warnings.
+#PHPUNIT=phpunit-8.5.4.phar
+
+# Oldest major version that supports PHP 7.2
+PHPUNIT=phpunit-6.5.9.phar
+
+if [ -z "$PHP_BASE"] ; then
+  echo "Usage: test.sh <php-base>"
+  exit 1
+fi
+
+if [ ! -x $PHP ]; then
+  echo Could not find executable file: $PHP
+fi
+
+# Download phpunit.
+[ -f $PHPUNIT ] || wget https://phar.phpunit.de/$PHPUNIT
+
+./generate_protos.sh
 
 # Compile c extension
-/bin/bash ./compile_extension.sh $VERSION
+pushd  ../ext/google/protobuf
+$PHP_BASE/bin/phpize
+./configure --with-php-config=$PHP_BASE/bin/php-config
+make clean && make -j$(nproc)
+popd
 
-tests=( array_test.php encode_decode_test.php generated_class_test.php map_field_test.php well_known_test.php descriptors_test.php wrapper_type_setters_test.php)
-
-for t in "${tests[@]}"
-do
-  echo "****************************"
-  echo "* $t"
-  echo "****************************"
-  php -dextension=../ext/google/protobuf/modules/protobuf.so `which phpunit` --bootstrap autoload.php $t
-  echo ""
-done
-
-for t in "${tests[@]}"
-do
-  echo "****************************"
-  echo "* $t persistent"
-  echo "****************************"
-  php -d protobuf.keep_descriptor_pool_after_request=1 -dextension=../ext/google/protobuf/modules/protobuf.so `which phpunit` --bootstrap autoload.php $t
-  echo ""
-done
+$PHP -dextension=../ext/google/protobuf/modules/protobuf.so $PHPUNIT --bootstrap autoload.php .
+$PHP -d protobuf.keep_descriptor_pool_after_request=1 -dextension=../ext/google/protobuf/modules/protobuf.so $PHPUNIT --bootstrap autoload.php .
 
 # # Make sure to run the memory test in debug mode.
 # php -dextension=../ext/google/protobuf/modules/protobuf.so memory_leak_test.php
 
 export ZEND_DONT_UNLOAD_MODULES=1
 export USE_ZEND_ALLOC=0
-valgrind --leak-check=yes php -dextension=../ext/google/protobuf/modules/protobuf.so memory_leak_test.php
-valgrind --leak-check=yes php -d protobuf.keep_descriptor_pool_after_request=1 -dextension=../ext/google/protobuf/modules/protobuf.so memory_leak_test.php
+valgrind --leak-check=yes $PHP -dextension=../ext/google/protobuf/modules/protobuf.so memory_leak_test.php
+valgrind --leak-check=yes $PHP -d protobuf.keep_descriptor_pool_after_request=1 -dextension=../ext/google/protobuf/modules/protobuf.so memory_leak_test.php
+
+exit 0
 
 # TODO(teboring): Only for debug (phpunit has memory leak which blocks this beging used by
 # regresssion test.)
